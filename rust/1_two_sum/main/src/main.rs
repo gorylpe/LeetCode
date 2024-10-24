a7289454c9e52261eea8f7c213358165ec6406a1
Piotr Przestrzelski <gorylp71@gmail.com>
2024-10-24 11:11:17 +0200

    1_two_sum memory optimized

--- rust/1_two_sum/main/src/main.rs
+++ rust/1_two_sum/main/src/main.rs
@@ -2,18 +2,18 @@ struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut nums_i: Vec<_> = nums.iter().enumerate().collect();
-        nums_i.sort_by(|a, b| a.1.cmp(&b.1));
+        let mut indices = (0..nums.len()).collect::<Vec<_>>();
+        indices.sort_unstable_by(|&i, &j| nums[i].cmp(&nums[j]));
         let mut i = 0;
-        let mut j = nums_i.len() - 1;
+        let mut j = nums.len() - 1;
         while i != j {
-            let sum = nums_i[i].1 + nums_i[j].1;
+            let sum = nums[indices[i]] + nums[indices[j]];
             if sum > target {
                 j -= 1;
             } else if sum < target {
                 i += 1;
             } else {
-                return vec![nums_i[i].0 as i32, nums_i[j].0 as i32];
+                return vec![indices[i] as i32, indices[j] as i32];
             }
         }
         panic!("no two sums found")
